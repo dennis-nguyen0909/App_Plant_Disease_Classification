ee0f5b65ddce03b7699c28a406df1ec3cfdb17ea
Minh Duy <dennis.nguyen0909@gmail.com>
2025-03-05 14:08:08 +0700

    feat: integration gemini ai

--- App.tsx
+++ App.tsx
@@ -1,45 +1,45 @@
-import React, { useState } from 'react';
-import { View, Text, Button, Image, StyleSheet } from 'react-native';
-import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
 import axios from 'axios';
 
 export default function App() {
   const [imageUri, setImageUri] = useState(null);
   const [prediction, setPrediction] = useState<string>('');
   const [confidence, setConfidence] = useState<string>('');
+  const [advice, setAdvice] = useState<string>('');
+  const [listData, setListData] = useState<string[]>([]);
 
   // Hàm để chọn ảnh từ thư viện
   const selectImage = () => {
-    launchImageLibrary({ mediaType: 'photo' }, (response) => {
+    launchImageLibrary({mediaType: 'photo'}, response => {
       if (!response.didCancel && !response.error && response.assets) {
         const uri = response.assets[0].uri;
+
+        // Clear the previous state before setting the new image
         setImageUri(uri);
+        setPrediction('');
+        setConfidence('');
+        setAdvice('');
+        setListData([]); // Clear the list when a new image is selected
       }
     });
   };
-
-  // Hàm để chụp ảnh bằng camera
-  const takePhoto = () => {
-  launchCamera({ mediaType: 'photo' }, (response) => {
-    if (response.didCancel) {
-      console.log('User cancelled camera');
-    } else if (response.errorCode) {
-      console.log('Camera error: ', response.errorMessage);
-    } else if (response.assets) {
-      const uri = response.assets[0].uri;
-      setImageUri(uri);
-    }
-  });
-};
-
 
   // Hàm để gọi API dự đoán
   const predictImage = async () => {
     if (!imageUri) return;
 
     const formData = new FormData();
-    
-    // Đảm bảo rằng đường dẫn URI có tiền tố "file://"
+
     formData.append('image', {
       uri: imageUri.startsWith('file://') ? imageUri : `file://${imageUri}`,
       type: 'image/jpeg',
@@ -47,27 +47,88 @@ export default function App() {
     });
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/predict', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
-      setPrediction(response.data.predicted_class);
-      setConfidence(response?.data?.confidence)
+      const response = await axios.post(
+        'http://127.0.0.1:8080/api/v1/predict',
+        formData,
+        {
+          headers: {'Content-Type': 'multipart/form-data'},
+        },
+      );
+      if (response.data) {
+        await handleAskGeminiAI(
+          `Tình trạng lá cây đang bị ${response?.data?.predicted_class}. Bạn hãy đưa ra lời khuyên`,
+        );
+        setPrediction(response.data.predicted_class);
+        setConfidence(response?.data?.confidence);
+
+        // Add prediction and confidence to the list
+        setListData(prevList => [
+          ...prevList,
+          `Tình trạng: ${response.data.predicted_class}`,
+          `Phần trăm dự đoán: ${response?.data?.confidence}`,
+        ]);
+      }
     } catch (error) {
       console.error(error);
       setPrediction('Error occurred during prediction');
     }
   };
 
+  const handleAskGeminiAI = async (question: string) => {
+    const url =
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBCc-hUh7vGCWQ6W6MO4Cosq0qM6MyljKg';
+
+    try {
+      const data = {
+        contents: [
+          {
+            parts: [{text: question}],
+          },
+        ],
+      };
+
+      const res = await axios.post(url, data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (res?.data) {
+        setAdvice(res?.data?.candidates[0].content?.parts[0]?.text);
+
+        // setListData(prevList => [
+        //   ...prevList,
+        //   `Lời khuyên: ${res?.data?.candidates[0].content?.parts[0]?.text}`,
+        // ]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Render function for FlatList
+  const renderItem = ({item}: {item: string}) => (
+    <Text style={styles.listItem}>{item}</Text>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Image Recognition App</Text>
-      {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
-      <Button title="Select Image" onPress={selectImage} />
-      {/* <Button title="Take Photo" onPress={takePhoto} /> */}
-      <Button title="Predict" onPress={predictImage} />
-      {prediction && <Text style={styles.prediction}>Tình trạng: {prediction === 'Healthy' ? 'Khỏe mạnh':prediction ==='Powdery' ? 'Bị nấm':'Rỉ sét'}</Text>}
-      {confidence && <Text style={styles.prediction}>Phần trăm dự đoán: {confidence}</Text>}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Image Recognition App</Text>
+        {imageUri && <Image source={{uri: imageUri}} style={styles.image} />}
+        <Button title="Select Image" onPress={selectImage} />
+        <Button title="Predict" onPress={predictImage} />
+        {prediction && <Text>Tình trạng lá: {prediction}</Text>}
+        {confidence && <Text>Phần trăm: {confidence}</Text>}
+        {listData && (
+          <FlatList
+            data={listData}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.list}
+          />
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -87,9 +148,13 @@ const styles = StyleSheet.create({
     height: 300,
     marginBottom: 20,
   },
-  prediction: {
-    fontSize: 18,
-    color: 'green',
+  list: {
     marginTop: 20,
+    width: '80%',
+  },
+  listItem: {
+    fontSize: 16,
+    color: '#000',
+    marginVertical: 5,
   },
 });
---
+++ src/utils/index.ts
@@ -0,0 +1,24 @@
+import {PermissionsAndroid} from 'react-native';
+
+async function requestCameraPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: 'Camera Permission',
+        message: 'This app needs access to your camera to take photos.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the camera');
+    } else {
+      console.log('Camera permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+export default requestCameraPermission;
